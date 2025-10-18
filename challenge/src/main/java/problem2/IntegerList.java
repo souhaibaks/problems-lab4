ce977174105eaf7fd9cd3cebb4a9094a05ae6717
souhaib <Mohamed.AKSIKAS@um6p.ma>
2025-10-18 01:16:31 +0100

    problem2

--- challenge/src/main/java/problem2/IntegerList.java
+++ challenge/src/main/java/problem2/IntegerList.java
@@ -3,12 +3,16 @@ package problem2;
 public class IntegerList
 {
     int[] list; //values in the list
+    int currentSize;//the current size of the list
+    int currentNum;//the current number of integers in the list
     //-------------------------------------------------------
 //create a list of the given size
 //-------------------------------------------------------
     public IntegerList(int size)
     {
         list = new int[size];
+        currentSize=size;
+        currentNum=size;
     }
     //-------------------------------------------------------
 //fill array with integers between 1 and 100, inclusive
@@ -23,7 +27,57 @@ public class IntegerList
 //-------------------------------------------------------
     public void print()
     {
-        for (int i=0; i<list.length; i++)
+        for (int i=0; i<currentNum; i++)
             System.out.println(i + ":\t" + list[i]);
     }
+    public void increaseSize(){
+        currentSize=currentSize*2;
+        int[]newList = new int[currentSize];
+        System.arraycopy(list,0,newList,0,list.length);
+        list=newList;
+
+    }
+    public void addElement(int newVal){
+        if (currentNum<currentSize){
+            list[currentNum]=newVal;
+            currentNum++;
+        }
+        else{
+            increaseSize();
+            addElement(newVal);
+        }
+    }
+    public void removeFirst(int newVal){
+        int index=-1;
+        for(int i=0; i<list.length; i++){
+            if(list[i]==newVal){
+                index=i;
+                break;
+            }
+        }
+        if (index!=-1){
+            currentNum--;
+            for(int i=index; i<currentNum; i++){
+                list[i]=list[i+1];
+            }
+        }
+
+    }
+    public void removeALL(int newVal){
+        int index=-1;
+        for(int i=0; i<list.length; i++){
+            if(list[i]==newVal){
+                index=i;
+                if (index!=-1){
+                    currentNum--;
+                    for(int j=index; j<currentNum; j++){
+                        list[j]=list[j+1];
+                    }
+            }
+        }
+
+        }
+    }
+
+
 }
--- challenge/src/main/java/problem2/IntegerListTest.java
+++ challenge/src/main/java/problem2/IntegerListTest.java
@@ -34,12 +34,43 @@ public class IntegerListTest
             case 1:
                 System.out.println("How big should the list be?");
                 int size = scan.nextInt();
+                //size=6;
                 list = new IntegerList(size);
                 list.randomize();
+                //list.list= new int[]{1,2,3,1,6,7};
+
                 break;
             case 2:
                 list.print();
                 break;
+            case 3:
+                if (list!=null) {
+                    System.out.println("Enter the new value : ");
+                    int newVal = scan.nextInt();
+                    list.addElement(newVal);
+                }
+                else  {
+                    System.out.println("Create first the list");
+                }
+            case 4:
+                if (list!=null) {
+                    System.out.println("Enter the value you want to remove: ");
+                    int val = scan.nextInt();
+                    list.removeFirst(val);
+                }
+                else  {
+                    System.out.println("Create first the list");
+                }
+            case 5:
+                if (list!=null) {
+                    System.out.println("Enter the value you want to remove: ");
+                    int val = scan.nextInt();
+                    list.removeALL(val);
+                }
+                else  {
+                    System.out.println("Create first the list");
+                }
+
             default:
                 System.out.println("Sorry, invalid choice");
         }
@@ -54,6 +85,9 @@ public class IntegerListTest
         System.out.println("0: Quit");
         System.out.println("1: Create a new list (** do this first!! **)");
         System.out.println("2: Print the list");
+        System.out.println("3: Add an element to  the list");
+        System.out.println("4: Remove the first occurrence  of a value from the list");
+        System.out.println("5: Remove the all occurrences  of a value from the list");
         System.out.print("\nEnter your choice: ");
     }
 }
